f49651017a7665ad596dfa6c84ff3ad7e0f16a8e
mxs <1721748881@qq.com>
2024-03-14 16:34:38 +0800

    一站式用户平台管理中心

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,23 @@
+import {GithubOutlined} from '@ant-design/icons';
+import {DefaultFooter} from '@ant-design/pro-layout';
+
+const Footer: React.FC = () => {
+  const defaultMessage = '雷莫出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: <><GithubOutlined/>雷莫 GitHub</>,
+          href: 'https://github.com/Lei-Mo',
+          blankTarget: true,
+        },
+
+      ]}
+    />
+  );
+};
+
+export default Footer;
---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SYSTEM_LOGO = "https://leimo-picgo.oss-cn-chengdu.aliyuncs.com/picgoimg/user-center-logo.png";
+
+/**
+ * 知识星球文档地址
+ */
+export const PLANET_LINK = "https://docs.qq.com/doc/DUG93dVNHbVZjZXpo";
